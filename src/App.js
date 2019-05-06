fec8241d047784cf2e27e4a45028458f0af70e7a
workingjubilee <workingjubilee@gmail.com>
2019-05-06 00:04:12 -0700

    Moves BG to app div

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+import Dashboard from './Dashboard/';
+
 
 function App() {
   return (
     <main>
-      <p>...</p>
+      <Dashboard />
     </main>
   );
 }
